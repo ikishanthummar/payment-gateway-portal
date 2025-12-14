93b9f0d2000ab9acc80d854b4b1e2c78e8264c09
Kishan Thummar <thummarkishan888@gmail.com>
2025-12-14 14:53:22 +0530

    ui changes

--- src/app/pages/transactions-component/transactions-component.spec.ts
+++ src/app/pages/transactions-component/transactions-component.spec.ts
@@ -1,23 +1,87 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator, PageEvent } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { Transaction } from '../../models/transaction/transaction.model';
+import { TransactionService } from '../../services/transaction/transaction-service';
 
-import { TransactionsComponent } from './transactions-component';
+@Component({
+  selector: 'app-transactions-component',
+  templateUrl: './transactions-component.html',
+  styleUrl: './transactions-component.scss'
+})
+export class TransactionsComponent implements OnInit {
 
-describe('TransactionsComponent', () => {
-  let component: TransactionsComponent;
-  let fixture: ComponentFixture<TransactionsComponent>;
+  displayedColumns = [
+    'transactionNumber',
+    'orderNumber',
+    'providerReference',
+    'amount',
+    'status',
+    'createdOn',
+    'updatedOn'
+  ];
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [TransactionsComponent]
-    })
-    .compileComponents();
+  transactions: Transaction[] = [];
 
-    fixture = TestBed.createComponent(TransactionsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  page = 1;
+  pageSize = 10;
+  totalRecords = 0;
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
-});
+  search = '';
+  sortBy = 'createdOn';
+  isDescending = true;
+
+  loading = false;
+
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  constructor(private transactionService: TransactionService) {}
+
+  ngOnInit(): void {
+    this.load();
+  }
+
+  load(): void {
+    this.loading = true;
+
+    this.transactionService.getPaged(
+      this.page,
+      this.pageSize,
+      this.search,
+      this.sortBy,
+      this.isDescending
+    ).subscribe({
+      next: res => {
+        this.transactions = res.items;
+        this.totalRecords = res.totalRecords;
+        this.loading = false;
+      },
+      error: () => {
+        this.loading = false;
+      }
+    });
+  }
+
+  onPageChange(event: PageEvent): void {
+    this.page = event.pageIndex + 1;
+    this.pageSize = event.pageSize;
+    this.load();
+  }
+
+  onSearch(): void {
+    this.page = 1;
+    this.paginator.firstPage();
+    this.load();
+  }
+
+  onSort(sort: Sort): void {
+    if (!sort.direction) return;
+
+    this.sortBy = sort.active;
+    this.isDescending = sort.direction === 'desc';
+
+    this.page = 1;
+    this.paginator.firstPage();
+    this.load();
+  }
+}
